c519fdebe3ca00936d37c0b8a51238dd98ac6be3
panchenko-kate <kmaslova_007@ukr.net>
2022-12-27 22:57:08 +0200

    first exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector('button[data-start]')
+const stopButton = document.querySelector('button[data-stop]')
+let bgColor = '';
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+    bgColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.setAttribute('disabled', 'disabled');
+}
+
+function onStopButtonClick() {
+    clearInterval(bgColor);
+    startButton.removeAttribute('disabled');
+}
